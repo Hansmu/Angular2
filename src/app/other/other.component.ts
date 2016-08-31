33c278a1697d1ddc5623b2a08222083b50f4d896
Hansmu <hans.miikael@gmail.com>
2016-08-31 13:39:41 +0300

    Property binding.

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'app-other',
-  templateUrl: 'other.component.html'
+  templateUrl: 'other.component.html',
+  styles: [`
+    .redBorder {
+      border: 1px solid red;
+    }
+  `]
 })
 export class OtherComponent {
   stringInterpolation = "This is string interpolation.";
